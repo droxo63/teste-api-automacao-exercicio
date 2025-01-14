442fb303703172fb1219a7324e2b8cdb4bb68ac1
Diego <droxo63@gmail.com>
2025-01-14 00:41:55 -0300

    update jenkinsfile

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,10 @@
 /// <reference types="cypress" />
 
 describe('Login', () => {
-before(() => {
-    cy.criarUsuario('Fulano', "fulano@qa.com","teste", "true" ).then(response => {
-        expect(response.status).equal(201)
-        expect(response.body.message).equal('Cadastro realizado com sucesso')
-      })
-});
 
     it('Deve fazer login com sucesso', () => {
-       
+
+
         cy.request({
             method: 'POST',
             url: 'login',
